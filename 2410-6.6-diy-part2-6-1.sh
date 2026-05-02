2512639c458873de311d85b83e646f49f4562770
zonghewang <diaozhaofeng@126.com>
2026-05-02 16:51:49 +0800

    Update IP, theme, hostname, and filename format

--- 2410-6.6-diy-part2-6-1.sh
+++ 2410-6.6-diy-part2-6-1.sh
@@ -11,13 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's|192.168.1.1|192.168.6.1|g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's|luci-theme-bootstrap|luci-theme-argon|g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
+sed -i 's|ImmortalWrt|ImmortalWrt-24.10-6.6-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")|g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
 sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-24.10-6.6-|' include/image.mk
