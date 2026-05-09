5d541b7fb5fdc9aecee952c317972011b8b34a87
xiaofengfeng <diaozhaofeng@126.com>
2026-05-09 20:24:07 +0800

    Disable hostname modification in DIY script

--- xiaomir3g/1806/1806-diy-part2-6-1.sh
+++ xiaomir3g/1806/1806-diy-part2-6-1.sh
@@ -17,7 +17,7 @@ sed -i 's|192.168.1.1|192.168.6.1|g' package/base-files/files/bin/config_generat
 sed -i 's|luci-theme-bootstrap|luci-theme-argon|g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's|ImmortalWrt|ImmortalWrt-18.06-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")|g' package/base-files/files/bin/config_generate
+# sed -i 's|ImmortalWrt|ImmortalWrt-18.06-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")|g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
 sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-18.06-|' include/image.mk
